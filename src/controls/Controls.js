e73e0e91698d9dd66edce77e3ef2e9aad63234cd
adkhiker <poirier.greg@gmail.com>
2019-12-08 18:38:37 -0500

    WIP: testing

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Controls = props => {
   const { locked, closed, toggleLocked, toggleClosed } = props;
 
   return (
     <div className="controls panel">
-      <button data-testid='lockBtn' disabled={!closed} onClick={toggleLocked} className="toggle-btn">
-        {locked ? 'Unlock Gate' : 'Lock Gate'}
+      <button
+        data-testid="lockBtn"
+        disabled={!closed}
+        onClick={toggleLocked}
+        className="toggle-btn"
+      >
+        {locked ? "Unlock Gate" : "Lock Gate"}
       </button>
-      <button data-testid='openBtn' disabled={locked} onClick={toggleClosed} className="toggle-btn">
-        {closed ? 'Open Gate' : 'Close Gate'}
+      <button
+        data-testid="openBtn"
+        disabled={locked}
+        onClick={toggleClosed}
+        className="toggle-btn"
+      >
+        {closed ? "Open Gate" : "Close Gate"}
       </button>
     </div>
   );
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,10 +3,10 @@ import React from "react";
 import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
 import Dashboard from "./Dashboard";
 
-describe('<Dashboard />', () => {
-    it('matches snapshot', () => {
-        const tree = renderer.create(<Dashboard />);
+describe("<Dashboard />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />);
 
-        expect(tree.toJSON()).toMatchSnapshot();
-    })
-})
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
